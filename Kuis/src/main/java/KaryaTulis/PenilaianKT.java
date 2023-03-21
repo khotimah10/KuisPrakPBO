6ca4d31aa80b66cbc7c39c0228c665c3816493f0
khotimah10 <113999675+khotimah10@users.noreply.github.com>
2023-03-21 12:30:50 +0700

    Add files via upload

---
+++ Kuis/src/main/java/KaryaTulis/PenilaianKT.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KaryaTulis;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class PenilaianKT {
+     String n, jr;
+     int nim;
+    
+     Scanner txtNamaKT = new Scanner(System.in);
+     Scanner txtJurusanKT = new Scanner(System.in);
+     Scanner txtNimKT = new Scanner(System.in);
+        
+     System.out.print("Input Nama : ");
+     n = nama.next();
+     System.out.print("Input Jurusan : ");
+     String txtJurusanKT = jr.next();
+     System.out.print("Input Nim : ");
+     int nim = txtNimKT.nextInt();
+                
+}
---
+++ Kuis/src/main/java/MainKategori.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lab Informatika
+ */
+
+public class MainKategori {
+    String nama, jr;
+    int nim;
+    
+    public static void main(String[]args){
+        
+        //Kategori
+        
+        //Penilaian Karya Tulis
+        //Biodata
+//        Scanner nama = new Scanner(System.in);
+//        Scanner jr = new Scanner(System.in);
+//        Scanner nim = new Scanner(System.in);
+//        
+//        System.out.println("Input Nama : ");
+//        String nm = nama.next();
+//        System.out.println("Input Jurusan : ");
+//        String jr = jr.next();
+//        System.out.println("Input Nim : ");
+//        int nim = nim.nextInt();
+                
+                
+                
+        //Penilaian 
+        
+        
+    }
+
+   
+
+}
